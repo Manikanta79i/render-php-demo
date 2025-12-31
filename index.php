7cda599afe99ce682aa89aa1dd1f9cb6b4d67de0
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2025-12-31 11:18:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "<h1>🚀 PHP App Successfully Deployed on Render!</h1>";
 
 echo "<p>Date: " . date('Y-m-d H:i:s') . "</p>";
-<?php
+
 $upi_id = "vathsi@sib";
 $name   = "Vathsi Fincorp";
 $amount = "1500";
